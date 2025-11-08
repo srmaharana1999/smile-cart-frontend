739755a128d5b1e4d3aeced0cc9b3e7325c9dbc2
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-11-09 02:43:23 +0530

    Implemented localization to smile cart

---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+
+import productApi from "apis/products";
+import { PageLoader } from "components/commons";
+import Header from "components/commons/Header";
+import ProductCard from "components/commons/ProductCard";
+import { MRP, OFFER_PRICE } from "components/constants";
+import { cartTotalOf } from "components/utils";
+import { NoData, Toastr } from "neetoui";
+import { isEmpty, keys } from "ramda";
+import useCartItemsStore from "stores/useCartItemsStore";
+
+import PriceCard from "./PriceCard";
+
+const Cart = () => {
+  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
+  const slugs = keys(cartItems);
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const totalMrp = cartTotalOf(products, MRP);
+  const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
+
+  const fetchCartProducts = async () => {
+    try {
+      const responses = await Promise.all(
+        slugs.map(slug => productApi.show(slug))
+      );
+      setProducts(responses);
+
+      responses.forEach(({ availableQuantity, name, slug }) => {
+        if (availableQuantity >= cartItems[slug]) return;
+
+        setSelectedQuantity(slug, availableQuantity);
+        if (availableQuantity === 0) {
+          Toastr.error(
+            `${name}is no longer available and has been removed from cart`,
+            { autoClose: 2000 }
+          );
+        }
+      });
+      console.log(responses);
+    } catch (error) {
+      console.log("An error occurred:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCartProducts();
+  }, [cartItems]);
+
+  if (isLoading) return <PageLoader />;
+
+  if (isEmpty(products)) {
+    return (
+      <>
+        <Header title="My Cart" />
+        <div className="flex h-screen items-center justify-center">
+          <NoData title="Your cart is empty!" />
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {" "}
+      <Header title="My Cart" />
+      <div className="mt-10 flex justify-center space-x-10">
+        <div className="w-1/3 space-y-5">
+          {products.map(product => (
+            <ProductCard key={product.slug} {...product} />
+          ))}
+        </div>
+        {totalMrp > 0 && (
+          <div className="w-1/4">
+            <PriceCard {...{ totalMrp, totalOfferPrice }} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Cart;
