31e9731cae9ab978ff2a42b0ec0e264ebdcedeb1
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-11-11 16:21:00 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "react";
-
-import productApi from "apis/products";
 import { PageLoader } from "components/commons";
 import Header from "components/commons/Header";
 import ProductCard from "components/commons/ProductCard";
 import { MRP, OFFER_PRICE } from "components/constants";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
-import { NoData, Toastr } from "neetoui";
+import { NoData } from "neetoui";
 import { isEmpty, keys } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
 import withTitle from "utils/withTitle";
@@ -15,43 +13,11 @@ import withTitle from "utils/withTitle";
 import PriceCard from "./PriceCard";
 
 const Cart = () => {
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
-  const slugs = keys(cartItems);
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
 
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
   const totalMrp = cartTotalOf(products, MRP);
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
-
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productApi.show(slug))
-      );
-      setProducts(responses);
-
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
-
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          Toastr.error(
-            `${name}is no longer available and has been removed from cart`,
-            { autoClose: 2000 }
-          );
-        }
-      });
-      console.log(responses);
-    } catch (error) {
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
 
   if (isLoading) return <PageLoader />;
 
